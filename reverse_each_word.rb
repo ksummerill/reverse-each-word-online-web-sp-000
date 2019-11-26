40f1faf5ed325138ab60373675e87351dee01723
Kate Summerill <kaitlynsummerill@gmail.com>
2019-11-26 15:18:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,12 +12,10 @@ def reverse_each_word(sentence)
 end
 
 
-# def reverse_each_word(sentence)
-#   backwards = []
-#   new_array = sentence.split
-#     new_array.each do |words|
-#       backwards << words.reverse!
-#     end
-#   new_sentence = backwards.join(" ")
-#   new_sentence
-# end
+def reverse_each_word(sentence)
+  backwards = []
+  new_array = sentence.split
+    new_array.collect do |words|
+      backwards << words.reverse!
+  end
+end
