a787f0483f06f963ce71b334a6773db3ee01ed78
Kate Summerill <kaitlynsummerill@gmail.com>
2019-11-26 15:29:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,21 +1,21 @@
 # sentence: "Hello there, and how are you?"
 # new_array: ["Hello", "there,", "and", "how", "are", "you?"]
 
-# def reverse_each_word(sentence)
-#   backwards = []
-#   new_array = sentence.split
-#     new_array.each do |words|
-#       backwards << words.reverse!
-#     end
-#   new_sentence = backwards.join(" ")
-#   new_sentence
-# end
-
-
 def reverse_each_word(sentence)
   backwards = []
   new_array = sentence.split
-    new_array.collect do |words|
+    new_array.each do |words|
       backwards << words.reverse!
-  end
+    end
+  new_sentence = backwards.join(" ")
+  new_sentence
 end
+
+
+# def reverse_each_word(sentence)
+#   backwards = []
+#   new_array = sentence.split
+#     new_array.collect do |words|
+#       backwards << words.reverse!
+#   end
+# end
