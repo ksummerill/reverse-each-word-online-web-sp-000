d74294d2858dea67ec714c02f66df30031240711
Kate Summerill <kaitlynsummerill@gmail.com>
2019-11-26 15:08:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,7 +7,7 @@ def reverse_each_word(sentence)
     new_array.each do |words|
       backwards << words.reverse!
     end
-  new_sentence = backwards.join
+  new_sentence = backwards.join(" ")
   new_sentence
 end
 
